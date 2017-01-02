133815e62288ad13e1756a6dcda7c7bc77702c63
dummypatch <dummypatch@gmail.com>
2017-01-02 13:35:44 +0900

    Add files via upload
    parsing 'dmesg' log files from 'dynamic_routing_overall'

---
+++ dmesg_parse_0167.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+#/****************************************************************************
+# only applied to default routing table for node placement 0167
+#****************************************************************************/ 
+
+
+if [ $# -eq 0 ]
+then
+echo "[ERROR] no arguments"
+exit
+fi
+
+
+LIST_1HOP=(
+	"0to1"
+	"1to0"
+	"0to6"
+	"6to0"
+	"1to7"
+	"7to1"
+	"6to7"
+	"7to6"
+)
+
+LIST_2HOP=(
+	"0to7"
+	"7to0"
+	"1to6"
+	"6to1"
+	)
+
+LIST_LOCAL=(
+	"0to0"
+	"1to1"
+	"6to6"
+	"7to7"
+	)
+
+
+FILENAME=$1
+
+
+for (( ii=0 ; ii<${#LIST_1HOP[*]} ; ii++))
+do
+	echo " 1 HOP PATH "
+	SRC=`echo ${LIST_1HOP[${ii}]}| tr to " "|awk -F" " '{print $1}'`
+	DEST=`echo  ${LIST_1HOP[${ii}]}| tr to " "|awk -F" " '{print $2}'`
+	cat $FILENAME |grep "src(${SRC})->dest(${DEST})"|grep -i dram
+	echo -e ""
+done
+
+for (( ii=0 ; ii<${#LIST_2HOP[*]} ; ii++))
+do
+	echo " 2 HOP PATH "
+	SRC=`echo ${LIST_2HOP[${ii}]}| tr to " "|awk -F" " '{print $1}'`
+	DEST=`echo  ${LIST_2HOP[${ii}]}| tr to " "|awk -F" " '{print $2}'`
+	cat $FILENAME |grep "src(${SRC})->dest(${DEST})"|grep -i dram
+	echo -e ""
+done
+
+
+for (( ii=0 ; ii<${#LIST_LOCAL[*]} ; ii++))
+do
+	echo " LOCAL PATH "
+	SRC=`echo ${LIST_LOCAL[${ii}]}| tr to " "|awk -F" " '{print $1}'`
+	DEST=`echo  ${LIST_LOCAL[${ii}]}| tr to " "|awk -F" " '{print $2}'`
+	cat $FILENAME |grep "src(${SRC})->dest(${DEST})"|grep -i dram
+	echo -e ""
+done
+
+
---
+++ extract_dmesg_log.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ "$1" == "" ];then
+	echo "USAGE : $0 file"
+	exit;
+fi
+
+export IFS='%'
+CUR_FILE=$1
+TEMP_FILE="qawsedrftg_aqswdefrgt.txt"
+
+START="`grep -i "input is  2" $CUR_FILE |tail -n 1`"
+END="`grep -i "input is  5" $CUR_FILE |tail -n 1`"
+
+START=$(printf '%s'  "${START}" |awk '{ gsub(/\[/,"\\["); print $0}' |awk '{ gsub(/\]/,"\\]"); print $0}' )
+END=$(printf '%s'  "${END}" |awk '{ gsub(/\[/,"\\["); print $0}' |awk '{ gsub(/\]/,"\\]"); print $0}')
+#printf '%s\n' "${START}"
+#printf '%s\n' "${END}"
+cat $CUR_FILE |sed -r '/'${START}'/,/'${END}'/!d' >> ${TEMP_FILE}
+mv ${TEMP_FILE} ${CUR_FILE}
+#echo ${CUR_FILE} is done.
