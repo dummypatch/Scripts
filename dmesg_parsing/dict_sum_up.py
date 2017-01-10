e434b63f783bf36adf995dc167e5645a732502a6
rootfriend <rootfriend@intel.E5>
2017-01-10 15:09:40 +0900

    nop

---
+++ dmesg_parsing/dict_sum_up.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+""" sum_up.py """
+import sys
+import operator
+import copy
+
+
+"""
+sums up data for all indices
+"""
+def sum_up(filepath,arg):
+	with open(filepath) as f:
+		sum = 0
+		hop = 0
+		array = []
+		for line in f:
+			# split up each line into a list
+			elems = line.split()
+			if len(elems) == 3:
+				if int(elems[-3]) == 1:
+					hop = 1
+				elif int(elems[-3]) == 2:
+					hop = 2
+			elif len(elems) == 2:
+				if elems[-2] == "LOCAL":
+					hop = "LOCAL"
+			# valid data line => 10 elements
+			# break point => 0 elements
+			# headers => no pattern
+			if len(elems) == 10:
+			# print index
+				if sum == 0:
+					temp1 = str(hop) + " hop path " + str(elems[-5])
+				sum = sum + int(elems[-1])
+			elif len(elems) == 0: # line break
+				temp2 = " " + str(sum)
+				array.append(temp1 + temp2)
+				sum = 0
+		if int(arg) == 1 :
+			for line in array:
+				print line
+
+		dict_map = dict()	
+		for idx,line in enumerate(array):
+			elems = line.split()
+			if (str(elems[-5]) == '1') or (str(elems[-5]) == '2') :
+				dest = str(elems[-2])
+				if dest.split('->')[1] in dict_map.keys():
+					dict_map[dest.split('->')[1]] += int(elems[-1])
+				else:
+					dict_map[dest.split('->')[1]] = int(elems[-1])
+
+#		for key, value in dict_map.items() :
+#			print (key, value)
+		dict_map_percentage = dict()
+		dict_map_percentage = copy.deepcopy(dict_map)
+		total_acc = 0
+		for v in dict_map.values():
+			total_acc += v
+		for k in dict_map.keys():
+			dict_map_percentage[k] = float(dict_map[k]) / float(total_acc) * float(100)
+		
+		max_key =  max(dict_map.iteritems(), key=operator.itemgetter(1))[0] 
+		print max_key , int(dict_map[max_key]) , '\tremote access ratio : {0:.2f}%'.format(dict_map_percentage[max_key])
+
+			
+
+if __name__ == "__main__":
+	if len(sys.argv) == 2 :
+		sum_up(sys.argv[1], 0 )
+	elif len(sys.argv) == 3 :
+		sum_up(sys.argv[1], sys.argv[2])
